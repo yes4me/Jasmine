2eab01d67ca13c7b02e7cfc5959bf7853df38c85
yes4me <yes4me@hotmail.com>
2015-07-27 01:55:37 -0700

    functions.js.is now designed as an object.to practice before() and after()

--- spec/basic-tests.js
+++ spec/basic-tests.js
@@ -6,24 +6,37 @@ Purpose:	Learning Jasmine - https://www.npmjs.com/package/jasmine-expect
 PS:			https://www.safaribooksonline.com/library/view/javascript-testing-with/9781449356729/Custom_Matchers.html#Custom_Matchers
 =========================================================================== */
 
+jasmine.getEnv().defaultTimeoutInterval = 3000;
+
 
 describe("basic", function() {
+	it("DIFFERENCE: toEqual (equality) vs toBe(equality & same object)", function() {
+		var value0 = 0;
+		var array1 = [1,2,3];
+		var array2 = [1,2,3];
+
+		expect(value0).toBeFalsy();							//.toBeTruthy()/.toBeFalsy() check for the actual value of true or false
+		expect(value0).not.toEqual(false);					//.toEqual() checks for equivalence of (==)
+
+		expect(array1).toEqual(array2);
+		expect(array1==array2).toBe(false);					//.toBe() checks for equivalence and same object (===)
+	});
+	it("expect(...).toEqual(...)", function() {
+		expect("abc").toEqual("abc");
+		expect(5).toEqual(5);								//expect(...).toEqual(Jasmine) == assert.equal(QUnit)
+		expect(2+3).toEqual(5);
+		expect(2+3).not.toEqual("5");
+		expect(0==false).toEqual(true);
+		expect(null==undefined).toEqual(true);
+	});
 	it("expect(...).toBe(...)", function() {
-		expect(true).toBe(true);							//expect(...).toBe(Jasmine) == assert.equal(QUnit)
+		expect(true).toBe(true);							//expect(...).toBe(Jasmine) === assert.deepEqual(QUnit)
 		expect("abc"=="abc").toBe(true);
 		expect(5==5).toBe(true);
 		expect(2+3==5).toBe(true);
 		expect(2+3=='5').toBe(true);
 		expect(0==false).toBe(true);
 		expect(null==undefined).toBe(true);
-	});
-	it("expect(...).toEqual(...)", function() {
-		expect("abc").toEqual("abc");
-		expect(5).toEqual(5);								//expect(...).toEqual(Jasmine) === assert.deepEqual(QUnit)
-		expect(2+3).toEqual(5);
-		expect(2+3).not.toEqual("5");
-		expect(0==false).toEqual(true);
-		expect(null==undefined).toEqual(true);
 	});
 	xit("How to skip a test", function() {
 		expect( sum(1,2,3) ).toEqual(6);
@@ -39,12 +52,22 @@ describe("basic", function() {
 		expect(function(){ sum() }).toThrow();
 		expect(function(){ sum("") }).toThrow();
 	});
-	it("OTHERS: true or false", function() {
+	it("OTHERS: what is FALSE(by default) in Javascript", function() {
+		expect(0).toBeFalsy();
+		expect(false).toBeFalsy();
+		expect("").toBeFalsy();
+		expect(null).toBeFalsy();			//This is different in QUnit
+		expect(NaN).toBeFalsy();			//This is different in QUnit
+		expect(undefined).toBeFalsy();		//This is different in QUnit
+	});
+	it("OTHERS", function() {
+		expect(0).toBeDefined();
+		expect(0).not.toBeTruthy();
 		expect(1).not.toBe(true);
 		expect(1).not.toBe(false);
 		expect(1).toBeTruthy();
-		expect(0).not.toBeTruthy();
-		expect(0).toBeFalsy();
-		expect(0).toBeDefined();
+		expect([1,2,3,4]).toContain(3);
+		expect("hello world").toContain("world");
+		expect("hello World").not.toContain("world");
 	});
 });
--- spec/functions-tests.js
+++ spec/functions-tests.js
@@ -9,97 +9,144 @@ Goal:
 	you test data.
 =========================================================================== */
 
+var myArray;
+
+
 
 describe("removeDuplicates Tests", function() {
-	it("special cases", function() {
-		expect(function(){ removeDuplicates() }).toThrow();
-		expect(function(){ removeDuplicates('a') }).toThrow();
-		expect(function(){ removeDuplicates([1,2,3], [1,2,3]) }).toThrow();
+	beforeEach(function() {
+		console.log("beforeEach: Starting removeDuplicates test");
+		myArray = new MyArray();
+	});
+	afterEach(function() {
+		console.log("afterEach: test removeDuplicates completed");
+		myArray = null;
+	});
+
+	it("special cases: MyArray is defined", function() {
+		myArray = new MyArray([1,2,3,1]);
+		expect( myArray.removeDuplicates() ).toEqual([1,2,3]);
+		expect( myArray.removeDuplicates('a') ).toEqual([1,2,3]);
+
+		myArray.setArray([5,4,2,2,3,1]);
+		expect( myArray.removeDuplicates() ).toEqual([5,4,2,3,1]);
+		expect( myArray.removeDuplicates('a') ).toEqual([5,4,2,3,1]);
+
+		myArray.setArray('asd');
+		expect( myArray.removeDuplicates() ).toEqual([5,4,2,3,1]);
+		expect( myArray.removeDuplicates('a') ).toEqual([5,4,2,3,1]);
+	});
+	it("special cases: MyArray is undefined", function() {
+		expect(function(){ myArray.removeDuplicates() }).toThrow();
+		expect(function(){ myArray.removeDuplicates('a') }).toThrow();
+		expect(function(){ myArray.removeDuplicates([]) }).toThrow();
+		expect( myArray.removeDuplicates([1,2,3], 'xxx', [4,5,6]) ).toEqual([1,2,3]);
 	});
 	it("no duplicate value", function() {
-		expect( removeDuplicates([1,2,3,4,5]) ).toEqual([1,2,3,4,5]);
-		expect( removeDuplicates([-1,-2,-3,-4,-5]) ).toEqual([-1,-2,-3,-4,-5]);
-		expect( removeDuplicates([-1,2,-3,4,8]) ).toEqual([-1,2,-3,4,8]);
-		expect( removeDuplicates([-1.4,2.9,-3.2,4.4,-2.9]) ).toEqual([-1.4,2.9,-3.2,4.4,-2.9]);
+		expect( myArray.removeDuplicates([1,2,3,4,5]) ).toEqual([1,2,3,4,5]);
+		expect( myArray.removeDuplicates([-1,-2,-3,-4,-5]) ).toEqual([-1,-2,-3,-4,-5]);
+		expect( myArray.removeDuplicates([-1,2,-3,4,8]) ).toEqual([-1,2,-3,4,8]);
+		expect( myArray.removeDuplicates([-1.4,2.9,-3.2,4.4,-2.9]) ).toEqual([-1.4,2.9,-3.2,4.4,-2.9]);
 	});
 	it("duplicate values", function() {
-		expect( removeDuplicates([-1,-2,-3,-2,-5]) ).toEqual([-1,-2,-3,-5]);
-		expect( removeDuplicates([-1,2,-3,4,2]) ).toEqual([-1,2,-3,4]);
-		expect( removeDuplicates([-1.4,2.9,-3.2,4.4,2.9]) ).toEqual([-1.4,2.9,-3.2,4.4]);
-		expect( removeDuplicates([-1.4,2.9,2.9,2.9,-3.2,4.4,2.9]) ).toEqual([-1.4,2.9,-3.2,4.4]);
-		expect( removeDuplicates([1,1,2,2,3,3,2,3,1,3]) ).toEqual([1,2,3]);
+		expect( myArray.removeDuplicates([-1,-2,-3,-2,-5]) ).toEqual([-1,-2,-3,-5]);
+		expect( myArray.removeDuplicates([-1,2,-3,4,2]) ).toEqual([-1,2,-3,4]);
+		expect( myArray.removeDuplicates([-1.4,2.9,-3.2,4.4,2.9]) ).toEqual([-1.4,2.9,-3.2,4.4]);
+		expect( myArray.removeDuplicates([-1.4,2.9,2.9,2.9,-3.2,4.4,2.9]) ).toEqual([-1.4,2.9,-3.2,4.4]);
+		expect( myArray.removeDuplicates([1,1,2,2,3,3,2,3,1,3]) ).toEqual([1,2,3]);
 	});
 });
 
 describe("hasDuplicates Tests", function() {
+	beforeEach(function() {
+		console.log("beforeEach: Starting removeDuplicates test");
+		myArray = new MyArray();
+	});
+	afterEach(function() {
+		console.log("afterEach: test removeDuplicates completed");
+		myArray = null;
+	});
+
 	it("special cases", function() {
-		expect(function(){ hasDuplicates() }).toThrow();
-		expect(function(){ hasDuplicates('a') }).toThrow();
-		expect(function(){ hasDuplicates([1,2,3], [1,2,3]) }).toThrow();
+		expect(function(){ myArray.hasDuplicates() }).toThrow();
+		expect(function(){ myArray.hasDuplicates('a') }).toThrow();
+		expect(function(){ myArray.hasDuplicates([]) }).toThrow();
+		expect( myArray.hasDuplicates([1,2,3], 'xxx', [4,5,6]) ).toBeFalsy();
 	});
 	it("no duplicate value", function() {
-		expect( hasDuplicates([2,4,3,1]) ).toBeFalsy();
-		expect( hasDuplicates([-2,-4,-3,-1]) ).toBeFalsy();
-		expect( hasDuplicates([-2,-5,1,9]) ).toBeFalsy();
-		expect( hasDuplicates([-2.3,5.34,1.03,6.9,5.35,9]) ).toBeFalsy();
+		expect( myArray.hasDuplicates([2,4,3,1]) ).toBeFalsy();
+		expect( myArray.hasDuplicates([-2,-4,-3,-1]) ).toBeFalsy();
+		expect( myArray.hasDuplicates([-2,-5,1,9]) ).toBeFalsy();
+		expect( myArray.hasDuplicates([-2.3,5.34,1.03,6.9,5.35,9]) ).toBeFalsy();
 	});
 	it("duplicate values", function() {
-		expect( hasDuplicates([2,1,3,1]) ).toBeTruthy();
-		expect( hasDuplicates([-2,-1,-3,-1]) ).toBeTruthy();
-		expect( hasDuplicates([-2,-5,-5,9]) ).toBeTruthy();
-		expect( hasDuplicates([-2.3,5.34,1.03,6.9,5.34,9]) ).toBeTruthy();
+		expect( myArray.hasDuplicates([2,1,3,1]) ).toBeTruthy();
+		expect( myArray.hasDuplicates([-2,-1,-3,-1]) ).toBeTruthy();
+		expect( myArray.hasDuplicates([-2,-5,-5,9]) ).toBeTruthy();
+		expect( myArray.hasDuplicates([-2.3,5.34,1.03,6.9,5.34,9]) ).toBeTruthy();
 	});
 });
 
 describe("areConsecutive Tests", function() {
+	beforeEach(function() {
+		console.log("beforeEach: Starting removeDuplicates test");
+		myArray = new MyArray();
+	});
+	afterEach(function() {
+		console.log("afterEach: test removeDuplicates completed");
+		myArray = null;
+	});
+
 	it("special cases", function() {
-		expect(function(){ areConsecutive() }).toThrow();
-		expect(function(){ areConsecutive('a') }).toThrow();
+		expect(function(){ myArray.areConsecutive() }).toThrow();
+		expect(function(){ myArray.areConsecutive('a') }).toThrow();
+		expect(function(){ myArray.areConsecutive([]) }).toThrow();
+		expect( myArray.areConsecutive([1,2,3], 'xxx', [4,5,6]) ).toBeTruthy();
 	});
 	it("positive values", function() {
-		expect( areConsecutive([4]) ).toBeTruthy();
-		expect( areConsecutive([3,4]) ).toBeTruthy();
-		expect( areConsecutive([4,3]) ).toBeTruthy();
-		expect( areConsecutive([4,4]) ).toBeFalsy();
-		expect( areConsecutive([2,3,4]) ).toBeTruthy();
-		expect( areConsecutive([4,3,2]) ).toBeTruthy();
-		expect( areConsecutive([3,4,2]) ).toBeFalsy();
-		expect( areConsecutive([4,2,3]) ).toBeFalsy();
-		expect( areConsecutive([4,4,4]) ).toBeFalsy();
+		expect( myArray.areConsecutive([4]) ).toBeTruthy();
+		expect( myArray.areConsecutive([3,4]) ).toBeTruthy();
+		expect( myArray.areConsecutive([4,3]) ).toBeTruthy();
+		expect( myArray.areConsecutive([4,4]) ).toBeFalsy();
+		expect( myArray.areConsecutive([2,3,4]) ).toBeTruthy();
+		expect( myArray.areConsecutive([4,3,2]) ).toBeTruthy();
+		expect( myArray.areConsecutive([3,4,2]) ).toBeFalsy();
+		expect( myArray.areConsecutive([4,2,3]) ).toBeFalsy();
+		expect( myArray.areConsecutive([4,4,4]) ).toBeFalsy();
 	});
 	it("negative values", function() {
-		expect( areConsecutive([-4]) ).toBeTruthy();
-		expect( areConsecutive([-3,-4]) ).toBeTruthy();
-		expect( areConsecutive([-4,-3]) ).toBeTruthy();
-		expect( areConsecutive([-4,-4]) ).toBeFalsy();
-		expect( areConsecutive([-2,-3,-4]) ).toBeTruthy();
-		expect( areConsecutive([-4,-3,-2]) ).toBeTruthy();
-		expect( areConsecutive([-3,-4,-2]) ).toBeFalsy();
-		expect( areConsecutive([-4,-2,-3]) ).toBeFalsy();
-		expect( areConsecutive([-4,-4,-4]) ).toBeFalsy();
+		expect( myArray.areConsecutive([-4]) ).toBeTruthy();
+		expect( myArray.areConsecutive([-3,-4]) ).toBeTruthy();
+		expect( myArray.areConsecutive([-4,-3]) ).toBeTruthy();
+		expect( myArray.areConsecutive([-4,-4]) ).toBeFalsy();
+		expect( myArray.areConsecutive([-2,-3,-4]) ).toBeTruthy();
+		expect( myArray.areConsecutive([-4,-3,-2]) ).toBeTruthy();
+		expect( myArray.areConsecutive([-3,-4,-2]) ).toBeFalsy();
+		expect( myArray.areConsecutive([-4,-2,-3]) ).toBeFalsy();
+		expect( myArray.areConsecutive([-4,-4,-4]) ).toBeFalsy();
 	});
 	it("positive and negative values", function() {
-		expect( areConsecutive([-1,0,1]) ).toBeTruthy();
-		expect( areConsecutive([1,0,-1]) ).toBeTruthy();
-		expect( areConsecutive([1,-1,-3]) ).toBeFalsy();
-		expect( areConsecutive([4.1,3.1,2.1]) ).toBeTruthy();
-		expect( areConsecutive([-4.1,-3.1,-2.1]) ).toBeTruthy();
-		expect( areConsecutive([-3.1,-4.1,-2.1]) ).toBeFalsy();
+		expect( myArray.areConsecutive([-1,0,1]) ).toBeTruthy();
+		expect( myArray.areConsecutive([1,0,-1]) ).toBeTruthy();
+		expect( myArray.areConsecutive([1,-1,-3]) ).toBeFalsy();
+		expect( myArray.areConsecutive([4.1,3.1,2.1]) ).toBeTruthy();
+		expect( myArray.areConsecutive([-4.1,-3.1,-2.1]) ).toBeTruthy();
+		expect( myArray.areConsecutive([-3.1,-4.1,-2.1]) ).toBeFalsy();
 	});
 	it("positive and negative values NOT allowing duplicates", function() {
-		expect( areConsecutive([0,1,2,2,3], false) ).toBeFalsy();
-		expect( areConsecutive([3,2,2,1,0], false) ).toBeFalsy();
-		expect( areConsecutive([-0,-1,-2,-2,-3], false) ).toBeFalsy();
-		expect( areConsecutive([-3,-2,-2,-1,-0], false) ).toBeFalsy();
-		expect( areConsecutive([-2,-1,0,1,1,2], false) ).toBeFalsy();
-		expect( areConsecutive([2,1,1,0,-1,-2], false) ).toBeFalsy();
+		expect( myArray.areConsecutive([0,1,2,2,3], false) ).toBeFalsy();
+		expect( myArray.areConsecutive([3,2,2,1,0], false) ).toBeFalsy();
+		expect( myArray.areConsecutive([-0,-1,-2,-2,-3], false) ).toBeFalsy();
+		expect( myArray.areConsecutive([-3,-2,-2,-1,-0], false) ).toBeFalsy();
+		expect( myArray.areConsecutive([-2,-1,0,1,1,2], false) ).toBeFalsy();
+		expect( myArray.areConsecutive([2,1,1,0,-1,-2], false) ).toBeFalsy();
 	});
 	it("positive and negative values allowing duplicates", function() {
-		expect( areConsecutive([0,1,2,2,3], true) ).toBeTruthy();
-		expect( areConsecutive([3,2,2,1,0], true) ).toBeTruthy();
-		expect( areConsecutive([-0,-1,-2,-2,-3], true) ).toBeTruthy();
-		expect( areConsecutive([-3,-2,-2,-1,-0], true) ).toBeTruthy();
-		expect( areConsecutive([-2,-1,0,1,1,2], true) ).toBeTruthy();
-		expect( areConsecutive([2,1,1,0,-1,-2], true) ).toBeTruthy();
+		expect( myArray.areConsecutive([0,1,2,2,3], true) ).toBeTruthy();
+		expect( myArray.areConsecutive([3,2,2,1,0], true) ).toBeTruthy();
+		expect( myArray.areConsecutive([-0,-1,-2,-2,-3], true) ).toBeTruthy();
+		expect( myArray.areConsecutive([-3,-2,-2,-1,-0], true) ).toBeTruthy();
+		expect( myArray.areConsecutive([-2,-1,0,1,1,2], true) ).toBeTruthy();
+		expect( myArray.areConsecutive([2,1,1,0,-1,-2], true) ).toBeTruthy();
 	});
 });
